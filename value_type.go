0aedf3ad69e2d612710bf2b677bbced4ef82170b
Joel Longtine <jlongtine@tangogroup.com>
2019-09-11 11:16:51 -0600

    Documenting which ValueTypes have been worked on

--- value_type.go
+++ value_type.go
@@ -2,16 +2,15 @@ package main
 
 // ValueType describes allowed values
 type ValueType struct {
-	AllowedValues       []string
-	AllowedPattern      string
-	AllowedPatternRegex string
+	AllowedValues       []string // √
+	AllowedPatternRegex string   // √
 	GetAtt              map[string]string
 	JSONMax             int `json:"JsonMax"`
 	ListMax             int
 	ListMin             int
-	NumberMax           float64
-	NumberMin           float64
+	NumberMax           float64 // √
+	NumberMin           float64 // √
 	Ref                 map[string][]string
-	StringMax           int
-	StringMin           int
+	StringMax           int // √
+	StringMin           int // √
 }
